8ef3f1d673ade7ea6f3b5a243b5aab4759a00291
Andrea Christian Memba <a.memba@alustudent.com>
2025-08-16 05:42:10 -0700

    Changed the appearance

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,5 @@
 for i in range(0, 100):
     if i < 10:
         print("{}{}, ".format(i, i), end = "")
-    elif i == 99:
-        print("{}".format(i))
     else:
         print("{}, ".format(i), end = "")
