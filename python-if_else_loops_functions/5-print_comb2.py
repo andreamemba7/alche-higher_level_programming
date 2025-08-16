147e0f4f7de4dd372b039477027d6317c5956efd
Andrea Christian Memba <a.memba@alustudent.com>
2025-08-16 05:31:46 -0700

    Changed the appearance of the output

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if i < 10:
-        print("{}{}".format(i, i))
+        print("{}{}, ".format(i, i), end = "")
     else:
-        print("{}".format(i))
+        print("{}, ".format(i), end = "")
