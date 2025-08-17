e4b652e5884172c8224e83f2f6aa00ed170069eb
Andrea Christian Memba <a.memba@alustudent.com>
2025-08-17 12:03:01 -0700

    Added space on each element

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end="")
+            print("{:2}".format(element), end="")
         print()
