012a0eaee5f55236ced72b2db34a7760f7319374
Andrea Christian Memba <a.memba@alustudent.com>
2025-08-17 13:19:29 -0700

    Made sure that the program detects the empty file first

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     max_value = my_list[0]
     for i in range(0, len(my_list)):
         if my_list[i] > max_value:
             max_value = my_list[i]
-        elif my_list is None:
-            return None
         else:
             max_value = max_value
     return max_value
