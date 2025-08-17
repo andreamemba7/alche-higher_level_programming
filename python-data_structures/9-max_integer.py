ec2606efc4fe614baa8afd702d67a54802bf9308
Andrea Christian Memba <a.memba@alustudent.com>
2025-08-17 13:16:29 -0700

    Made the max to be the first number

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = 0
+    max_value = my_list[0]
     for i in range(0, len(my_list)):
         if my_list[i] > max_value:
             max_value = my_list[i]
